9fb58fece6356f2af1a563394353e0ba0984f58c
Sergey <eich@tut.by>
2020-12-17 14:22:23 +0300

    подключил bootsrapt vue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+import "@/css/custom.scss";
+
 Vue.config.productionTip = false;
 
 new Vue({
