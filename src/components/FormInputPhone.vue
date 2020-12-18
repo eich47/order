b1d26ea1ad964c954df892a4ea7d026de12409da
Sergey <eich@tut.by>
2020-12-18 16:55:24 +0300

    секция телефон с описанием

---
+++ src/components/FormInputPhone.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="wrapper phone">
+    <div class="d-flex">
+      <div class="col-6 part1">
+        <b-form-input
+          type="tel"
+          v-model="tel"
+          :placeholder="placeholder"
+        ></b-form-input>
+      </div>
+      <div class="col-6 part2">
+        <p class="text">
+          {{ userTextPart1 }} <span>{{ userTextPart2 }}</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormInputPhone",
+  data() {
+    return {
+      tel: ""
+    };
+  },
+  props: {
+    placeholder: {
+      type: String,
+      required: true
+    },
+    userTextPart1: {
+      type: String,
+      required: true
+    },
+    userTextPart2: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.part1 {
+  padding-left: 0;
+}
+.part2 {
+  padding-right: 0;
+  display: flex;
+  align-items: center;
+}
+.part2 .text {
+  font-size: 0.8rem;
+  margin-bottom: 0;
+}
+.part2 .text span {
+  display: block;
+}
+</style>
--- src/components/FormUserInfo.vue
+++ src/components/FormUserInfo.vue
@@ -3,6 +3,11 @@
     <b-form>
       <FormSectionTitle text="Recipient" />
       <FormInputText placeholder="Full Name" />
+      <FormInputPhone
+        placeholder="DayTime Phone"
+        user-text-part1="For delivery"
+        user-text-part2="questions only"
+      />
       <b-button class="mt-5" type="submit" variant="primary">
         Continue
       </b-button>
@@ -13,9 +18,10 @@
 <script>
 import FormSectionTitle from "@/components/FormSectionTitle";
 import FormInputText from "@/components/FormInputText";
+import FormInputPhone from "@/components/FormInputPhone";
 export default {
   name: "FormUserInfo",
-  components: { FormInputText, FormSectionTitle }
+  components: { FormInputPhone, FormInputText, FormSectionTitle }
 };
 </script>
 
