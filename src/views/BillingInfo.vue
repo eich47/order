501b182c9a494b4fb30078692e939599ed36bc8c
Sergey <eich@tut.by>
2020-12-24 16:26:21 +0300

    заглушка для billingInfo

--- src/views/BillingInfo.vue
+++ src/views/BillingInfo.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    some info here
+    <TitleSection text="Billing Info" />
+    <p>some info here</p>
+
+    <b-form @submit.prevent="onSubmit">
+      <div class="col-6 pl-0 mt-3">
+        <b-button block type="submit" variant="primary" :disabled="false">
+          Continue
+        </b-button>
+      </div>
+    </b-form>
   </div>
 </template>
 
 <script>
+import TitleSection from "@/components/TitleSection";
 export default {
-  name: "BillingInfo"
+  name: "BillingInfo",
+  components: { TitleSection },
+  methods: {
+    onSubmit() {
+      console.log("onSumbit billing info");
+    }
+  }
 };
 </script>
 
