a526fcccff0f145682d6193ac4e48ca52844bb34
Sergey <eich@tut.by>
2020-12-24 14:27:07 +0300

    установил роутер

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <b-container>
-      <b-row>
-        <b-col sm="6">
-          <Navigation />
-
-          <ShoppingInfo />
-        </b-col>
-        <b-col sm="5">22</b-col>
-      </b-row>
-    </b-container>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import Navigation from "./components/Navigation";
-import ShoppingInfo from "@/components/ShoppingInfo";
-export default {
-  name: "App",
-  components: { ShoppingInfo, Navigation }
-};
-</script>
-
 <style lang="scss">
 #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
 import "@/css/custom.scss";
+import router from "./router";
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount("#app");
