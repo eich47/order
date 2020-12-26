c7b33f22f777c0f5350d26e04eae29170a9e03ca
Sergey <eich@tut.by>
2020-12-26 17:53:02 +0300

    выборочный импорт компонентов из bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
+import {
+  LayoutPlugin,
+  FormPlugin,
+  ButtonPlugin,
+  NavPlugin,
+  FormInputPlugin,
+  FormSelectPlugin
+} from "bootstrap-vue";
+Vue.use(LayoutPlugin);
+Vue.use(FormPlugin);
+Vue.use(ButtonPlugin);
+Vue.use(NavPlugin);
+Vue.use(FormInputPlugin);
+Vue.use(FormSelectPlugin);
 
 import "@/css/custom.scss";
 import router from "./router";
